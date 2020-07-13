30c15e27458e21e0d6fd4e26867ccee53a32539d
Aditya Mali <adityamali33@gmail.com>
2020-07-13 15:00:20 +0530

    NN with dot func

--- neural networks/PyNN.py
+++ neural networks/PyNN.py
@@ -1,3 +1,4 @@
+import numpy as np
 inputs = [1, 2,3,2.5]
 
 weights = [	[0.2,0.8,-0.5,1.0],
@@ -6,13 +7,13 @@ weights = [	[0.2,0.8,-0.5,1.0],
 
 bias1 = [2, 3, 0.5]
 
-layer_outputs = []
+# layer_outputs = []
 
-for w , b in zip(weights,bias1):
-	neuron_op = 0
-	for i , w1 in zip(inputs,w):
-		neuron_op += i * w1
-	neuron_op += b
-	layer_outputs.append(neuron_op)
+# for w , b in zip(weights,bias1):
+# 	neuron_op = 0
+# 	for i , w1 in zip(inputs,w): 
+# 		neuron_op += i * w1
+# 	neuron_op += b
+# 	layer_outputs.append(neuron_op)
 
-print(layer_outputs)
+print(np.dot(weights, inputs) + bias1)
