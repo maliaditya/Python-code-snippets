e88492828f3b2ef6e39e4b0d6203293662fb20db
Aditya Mali <41832893+maliaditya@users.noreply.github.com>
2020-09-04 16:25:59 +0530

    File Handling

---
+++ File_Handling/read&write.py
@@ -0,0 +1,3 @@
+a = "print(5+4,' is sum of 4 & 5 ')"
+
+exec(a)
--- File_Handling/writetofile.py
+++ File_Handling/writetofile.py
@@ -1,10 +1,20 @@
 import sys
+code = 'print(int(input())+int(input()))'
+
+input_part = [1,2]
+def input():
+            a = input_part[0]
+            del input_part[0]
+            return a
+
 a = sys.stdout
+
 sys.stdout = open('file.txt','w')
-print("hello world")
+exec(code)
 sys.stdout.close()
+
 sys.stdout = a
 
 output = open('file.txt','r').read()
-print(output)
+print("op ",output)
 
